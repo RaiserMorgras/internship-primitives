86dba00c9fed4839c2bc41cb5d80b69ffb7ec49e
RaiserMorgras <arseniygromovoy@gmail.com>
2020-02-05 15:16:45 +0200

    Little tweaks

--- primitives-task/src/primitivesDeciderTest/PrimitivesDecider.java
+++ primitives-task/src/primitivesDeciderTest/PrimitivesDecider.java
@@ -6,15 +6,15 @@ public class PrimitivesDecider {
         try {
             long parsedNum = Long.parseLong(num);
             if (Byte.MIN_VALUE <= parsedNum && parsedNum <= Byte.MAX_VALUE) {
-                countedTypes += 4;
+                countedTypes = 4;
             }
             else if (Short.MIN_VALUE <= parsedNum && parsedNum <= Short.MAX_VALUE) {
-                    countedTypes += 3;
+                    countedTypes = 3;
                 }
                 else if (Integer.MIN_VALUE <= parsedNum && parsedNum <= Integer.MAX_VALUE) {
-                        countedTypes += 2;
+                        countedTypes = 2;
                     }
-                    else countedTypes += 1;
+                    else countedTypes = 1;
         }
         catch (NumberFormatException e) {
             System.out.println("Provided input " + num + " is too big or not a number");
@@ -30,17 +30,15 @@ public class PrimitivesDecider {
         }
         else {
             result = num + " can be fitted in:";
-            if (countedTypes == 4) {
-                result += "\n" + "byte";
-            }
-            if (countedTypes >= 3) {
-                result += "\n" + "short";
-            }
-            if (countedTypes >= 2) {
-                result += "\n" + "int";
-            }
-            if (countedTypes >= 1) {
-                result += "\n" + "long";
+            switch (countedTypes) {
+                case 4:
+                    result += "\n" + "byte";
+                case 3:
+                    result += "\n" + "short";
+                case 2:
+                    result += "\n" + "int";
+                case 1:
+                    result += "\n" + "long";
             }
         }
         return result;
